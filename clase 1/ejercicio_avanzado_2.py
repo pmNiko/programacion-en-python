82cd66376bf77f7417a7b280ba6e1e3c50040120
pmNiko <nikolas090189@gmail.com>
2021-08-13 18:50:14 -0300

    Extension del Ejercicio 3

--- clase 1/ejercicio_avanzado_2.py
+++ clase 1/ejercicio_avanzado_2.py
@@ -1,39 +1,3 @@
-# Celeste — hoy a las 20:52
-""" nombre = input("ingrese su nombre: ")
-edad = input("ingrese su edad: ")
-direccion = input("ingrese su direccion: ")
-telefono = input("ingrese su telefono: ")
-
-dict_1 = {'nombre': nombre, 'edad': edad,
-          'direccion': direccion, 'telefono': telefono}
-print(dict_1)
-
-print(dict_1['nombre'], "tiene", dict_1['edad'], "años, vive en",
-      dict_1['direccion'], ", su numero de telefono es", dict_1['telefono']) """
-
-
-# Fausto — hoy a las 20:53
-""" nombre = input("ingrese su nombre")
-edad = input("ingrese su edad ")
-apellido = input("ingrese su apellido")
-telefono = input("ingrese su telefono ")
-mail = input("ingrese su mail")
-direccion = input("ingrese su direccion")
-dictionary = {"nombre": nombre, "edad": edad, "apellido": apellido,
-              "telefono": telefono, "mail": mail, "dirrecion": direccion}
-print(dictionary) """
-
-
-# Alex
-""" tu_nombre = input("Ingrese su nombre: ")
-edad = input("Ingrese su edad: ")
-direccion = input("Ingrese su direccion: ")
-telefono = input("Ingrese su numero de telefono: ")
-
-diccionario = {'nombre': tu_nombre , 'edad': edad , 'direccion': direccion , 'telefono': telefono}
-
-print("Su nombre es", diccionario['nombre'], ". Usted tiene", diccionario['edad'], "años, vive en", diccionario['direccion'], "y su numero de telefono es", diccionario['telefono']) """
-
 
 # Ejercicio 2
 
