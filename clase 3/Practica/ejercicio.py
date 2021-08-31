8ce179bf7430ccdc5b8148caae1ec4267b261c61
pmNiko <nikolas090189@gmail.com>
2021-08-31 19:51:38 -0300

    Estructura general de la practica

---
+++ clase 3/Practica/ejercicio.py
@@ -0,0 +1,36 @@
+""" Ejercicio en grupo """
+
+menu = """ 
+      Menu
+  1- Saludar usuario
+  2- Mostrar un numero aleatorio
+  3- Mostrar opcion elegida
+  4- Ver fecha actual
+  5- Login de usuario
+  6- Area de un rectangulo
+  7- Salir
+"""
+
+print(menu)
+
+opcion = 78
+
+while opcion != 7:
+    opcion = int(input("Ingrese una opción: "))
+
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        pass
+    elif opcion == 6:
+        pass
+    elif opcion == 7:
+        print("Saliendo del sistema...")
+    else:
+        print("Opción invvalida.")
