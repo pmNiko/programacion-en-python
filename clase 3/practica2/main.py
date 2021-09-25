14d882a38d9edc03d5b1b7568c5345bc5a2d0c51
sami3000 <cordobamolinasamira@gmail.com>
2021-09-25 11:23:46 -0300

    practica clase 3

---
+++ clase 3/practica2/main.py
@@ -0,0 +1,42 @@
+""" importacion de modulos """
+# from funciones import suma
+  
+menu=   """ Menu
+  1- Saludar usuario
+  2- Mostrar un numero aleatorio
+  3- Mostrar opcion elegida
+  4- Ver fecha actual
+  5- Login de usuario
+  6- Area de un rectangulo
+  7- Salir """
+  
+print(menu)
+
+while True: 
+    opcion= int(input("ingrese una opcion: "))
+    
+    if opcion == 1: 
+        # suma()
+        pass
+    
+    elif opcion == 2: 
+        pass
+    
+    elif opcion == 3: 
+        pass
+    
+    elif opcion == 4: 
+        pass
+    
+    elif opcion == 5: 
+        pass
+    
+    elif opcion == 6: 
+        pass
+    
+    elif opcion == 7: 
+        print("Saliendo del sistema.")
+        break
+    
+    else:
+        print("Opcion no valida.")
