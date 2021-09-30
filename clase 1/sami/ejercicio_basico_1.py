b8fc213faf4bd940dce0d7422cbf796eb4dabbd4
sami3000 <cordobamolinasamira@gmail.com>
2021-09-30 20:10:58 -0300

    carpeta ejercitacion clase1

---
+++ clase 1/sami/ejercicio_basico_1.py
@@ -0,0 +1,19 @@
+#Ejercicio basico 1)    
+
+nombre = input("Ingrese un nombre: ")           # me tira error, "samira no esta definido" rta: con el placeholder no da error. 
+print ("Nombre:", nombre)
+
+apellido= input("Ingrese su apellido: ")
+print ("Apellido:", apellido)
+print(f"apellido: {apellido}")
+edad= int(input("Ingrese su edad: "))
+print ("Edad:", edad)
+
+telefono= int(input("Ingrese su telefono"))           #es necesario aclarar en "int" en este caso?
+print ("Telefono:", telefono)
+
+email= input("Ingrese su E-mail: ")              #seria alfanumerico? como lo escribo? rta: tipo string
+print ("E-mail:", email)
+
+
+
