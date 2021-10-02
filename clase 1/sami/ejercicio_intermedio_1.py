97a91e0145841fde147687635bf074edeb6ced00
sami3000 <cordobamolinasamira@gmail.com>
2021-10-02 12:15:59 -0300

    ejercicio intermedio 1 terminado

---
+++ clase 1/sami/ejercicio_intermedio_1.py
@@ -0,0 +1,12 @@
+""" Ejercicio 1 
+  Crea una tupla con números, pide un número por teclado e indica cuantas veces se repite.
+
+"""  
+ 
+#  ejercicio 1: 
+
+numero = int(input("ingrese un numero"))
+
+tupla_numerica = 1, 2, 3 , 4 , 5, 20, 13, 6, 30, 2, 2, 2
+
+print(tupla_numerica.count(numero))
