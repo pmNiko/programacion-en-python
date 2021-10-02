9b0b1328fe273c285776c09be072d31bf566afe7
NachoBokita <nachobucchi2@gmail.com>
2021-10-02 12:30:13 -0300

    EjercitacionClase5

---
+++ clase 5/nacho/excepciones_1.py
@@ -0,0 +1,8 @@
+"""
+  Ejercicio 1
+  Localiza el error en el siguiente bloque de código. 
+  Maneja la excepción para evitar que el programa se bloquee y 
+  además explica en un mensaje al usuario la causa y/o solución:
+
+  resultado = 10/0
+"""
